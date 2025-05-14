3280a5cd841af91b0a60952f31105138430ecf7d
Aarush Pathak <apathak9@wisc.edu>
2025-05-14 06:16:33 +0400

    finish creating the databse model in model.py

---
+++ backend/models.py
@@ -0,0 +1,16 @@
+from config import db
+
+
+class contact(db.Model):
+    id = db.column(db.Integer, primary_key=True)
+    first_name = db.column(db.String(80), nullable=False, unique=False)
+    last_name = db.column(db.String(80), nullable=False, unique=False)
+    email = db.column(db.String(120), nullable=False, unique=True)
+
+    def to_json(self):
+        return {
+            'id': self.id,
+            'firstName': self.first_name,
+            'lastName': self.last_name,
+            'email': self.email
+        }
