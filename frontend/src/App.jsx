a69780062c404a536bf29ca4213d9d30272692da
Aarush Pathak <apathak9@wisc.edu>
2025-05-15 07:21:11 +0400

    implement and style a modal for creating/updating contact

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,47 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import ContactList from './ContactList'
-import ContactForm from './ContactForm'
+import { useState, useEffect } from "react";
+import "./App.css";
+import ContactList from "./ContactList";
+import ContactForm from "./ContactForm";
 
 function App() {
-  
-  const [contacts, setContacts] = useState([])
-  
+  const [contacts, setContacts] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
-    fetchContacts()
-  }, [])
+    fetchContacts();
+  }, []);
 
-  const fetchContacts = async () =>{
-    const response = await fetch('http://127.0.0.1:5000/contacts')
-    const data = await response.json()
-    setContacts(data.contacts)
-  }
+  const fetchContacts = async () => {
+    const response = await fetch("http://127.0.0.1:5000/contacts");
+    const data = await response.json();
+    setContacts(data.contacts);
+  };
 
-  return <>
-    <ContactList contacts={contacts} />
-    <ContactForm />
-  </>
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const openCreateModal = () => {
+    if (!isModalOpen) setIsModalOpen(true);
+  };
+
+  return (
+    <>
+      <button onClick={openCreateModal}>Create New Contact</button>
+      {isModalOpen && (
+        <div className="modal">
+          <div className="modal-content">
+            <span className="close" onClick={closeModal}>
+              &times;
+            </span>
+            <ContactForm />
+          </div>
+        </div>
+      )}
+      <ContactList contacts={contacts} />
+      <ContactForm />
+    </>
+  );
 }
 
-export default App
+export default App;
